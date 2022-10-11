2439a652ea8b978a189b43065bba37d625809d63
Iiro Kajaniemi <kajaniemi.iiro@gmail.com>
2022-10-11 10:04:02 +0300

    Create react app. Footer, Header, Layouts

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return <div>
+    <ul>
+      <li>Calendar</li>
+      <li></li>
+      <li>LOG IN / REGISTER</li>
+      <li></li>
+    </ul>
+  </div>
+}
+
+export default Header;
