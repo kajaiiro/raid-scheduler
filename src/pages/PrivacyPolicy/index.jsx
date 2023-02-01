0c0e99ba67e7141b920f101e2872cb1d0f2b6e8f
Iiro Kajaniemi <kajaniemi.iiro@gmail.com>
2023-02-01 18:13:04 +0200

    Add PrivacyPolicy page

---
+++ src/pages/PrivacyPolicy/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PrivacyPolicy() {
+    return (
+        <fragment>
+            <h1>Privacy Policy</h1>
+            Site does not collect anydata.
+        </fragment>
+    );
+}
